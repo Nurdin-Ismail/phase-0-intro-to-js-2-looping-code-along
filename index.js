d426a1797061afac01b0bd6be1ff25af079b1d14
Nurdin <nurdin.ismail@student.moringaschool.com>
2023-06-17 23:25:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const arr = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(arrlon, soru){
+    let lolu = []
+    for(let li = 0; li < arrlon.length; li++){
+        
+        lolu.push(`Thank you, ${arrlon[li]}, for the wonderful ${soru} gift!`)
+        
+
+    }
+    return lolu
+
+}
+
+ function countDown(minu){
+    
+    while(minu > 0 || 0 === 'nino'  ){
+        console.log(minu);
+        minu = minu - 1;
+
+    }
+    console.log(0)
+ }
